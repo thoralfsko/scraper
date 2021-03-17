2cb4c4465bb8dfbbdda57402ebe7891543caf6ec
Tim <timcstrauss@gmail.com>
2021-03-17 19:10:56 -0400

    grabbing linescores for each team works. However there is an issue when handling games with overtime. Im not sure how i want to resolve it yet... stay tuned

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
+from write import *
+
+#list of all teams
+teams = '''BOS MIL IND PHI BRK CLE ORL CHO ATL NYK MIA TOR
+    CHI WAS DET LAL LAC UTA PHO POR DAL SAS DEN OKC GSW MEM
+    SAC NOP HOU MIN'''.split()
+#set of gids that have been scraped
+links_processed = set()
+
 def main():
-    pass
+    #pull game data for every team
+    for team in teams:
+        #get game lins for team. only 2021 for now
+        links = get_game_links(team, '2021')
+
+        #process each game link
+        for link in links:
+            #check if link has been processed
+            if link not in links_processed:
+                #write linescore
+                write_line_score(link)
+
+                #add link to proccessed set
+                links_processed.add(link)
+
 
 if __name__ == '__main__':
     main()
