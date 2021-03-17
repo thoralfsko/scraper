014d39eb856536a4cff5f1cb14beab3efc14ebba
Tim <timcstrauss@gmail.com>
2021-03-17 12:03:09 -0400

    added docs for write. and changed test.py and docs.py to create the directorys if they do not exist

---
+++ docs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+from scraper import *
+from write import *
+
+#create the directory if it doesnt exist
+if os.path.isdir('docs/') == False:
+    os.mkdir('docs/')
+
+#create a list of methods in scraper.py
+methods = [get_game_links, get_line_score, get_four_factors, get_basic_box,
+    get_team_info, get_basic_table, get_basic_box_q1, get_basic_box_q2,
+    get_basic_box_q3, get_basic_box_q4, get_basic_box_h1, get_basic_box_h2]
+
+#create scraper docs
+file = open('docs/scraper.txt', 'w+')
+for i,method in enumerate(methods):
+    file.write(method.__doc__)
+    if len(methods) - 1 != i:
+        file.write('\n\n########################################\n')
+
+file.close()
+
+#create a list of methods from write.py
+methods = [write_line_score, create_directory, clean_directory, write_four_factors,
+    write_basic_box, write_basic_box_util, write_basic_box_q1, write_basic_box_q2,
+    write_basic_box_q3, write_basic_box_q4, write_basic_box_h1, write_basic_box_h2]
+
+#create write docs
+file = open('docs/write.txt', 'w+')
+for i,method in enumerate(methods):
+    file.write(method.__doc__)
+    if len(methods) - 1 != i:
+        file.write('\n\n########################################\n')
+
+file.close()
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os
 
+#create pytest directory if it does not exist
+if os.path.isdir('pytest/') == False:
+    os.mkdir('pytest/')
+
 #run tests and write output to pytest directory
 os.system('pytest -vv -s write.py > pytest/write.txt')
 os.system('pytest -vv -s scraper.py > pytest/scraper.txt')
